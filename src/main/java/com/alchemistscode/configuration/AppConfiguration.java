cbfebfedc2fc84d0a1d7e70acbcc55c096a74907
drupalio <pntra2.team@gmail.com>
2017-09-01 14:42:55 -0500

    adding new configuration class based on anotations

---
+++ src/main/java/com/alchemistscode/configuration/AppConfiguration.java
@@ -0,0 +1,11 @@
+package com.alchemistscode.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *  Drupalio
+ *  Configuration class for application
+ */
+@Configuration
+public class AppConfiguration {
+}
